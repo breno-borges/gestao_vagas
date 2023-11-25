c8ab895aba3eabf1bcbbb0baa3e6929670423403
Breno Borges <breno.borges.15@outlook.com>
2023-11-24 22:39:46 -0300

    Criado repositório que extende JpaRepository e verificação se o usuário já existe antes de criá-lo

---
+++ src/main/java/br/com/brenoborges/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,11 @@
+package br.com.brenoborges.gestao_vagas.modules.candidate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+}
--- src/main/java/br/com/brenoborges/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/brenoborges/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,19 +1,31 @@
 package br.com.brenoborges.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.brenoborges.gestao_vagas.exceptions.UserFoundException;
 import br.com.brenoborges.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.brenoborges.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato" + candidateEntity.getEmail());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+        return this.candidateRepository.save(candidateEntity);
     }
 }

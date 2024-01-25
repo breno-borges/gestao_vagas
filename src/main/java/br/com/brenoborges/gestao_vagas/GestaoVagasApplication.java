ad2470772f37d6f2053609b35286d0ed63d42ee3
Breno Borges <breno.borges.15@outlook.com>
2024-01-25 19:45:27 -0300

    Adicionado dependência SpringDoc para utilizar o Swagger e fazer a documentação da API

--- src/main/java/br/com/brenoborges/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/brenoborges/gestao_vagas/GestaoVagasApplication.java
@@ -3,7 +3,11 @@ package br.com.brenoborges.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestao de Vagas", description = "API responsavel pela gestao de vagas", version = "1.0"))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {

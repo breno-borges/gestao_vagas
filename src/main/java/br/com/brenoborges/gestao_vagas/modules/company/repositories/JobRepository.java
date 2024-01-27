5889432d6705850ab896904b133c7b7b1261a884
Breno Borges <breno.borges.15@outlook.com>
2024-01-27 17:08:58 -0300

    Adicionado método para procurar e listar os Jobs

--- src/main/java/br/com/brenoborges/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/brenoborges/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -1,5 +1,6 @@
 package br.com.brenoborges.gestao_vagas.modules.company.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import br.com.brenoborges.gestao_vagas.modules.company.entities.JobEntity;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
 
+    List<JobEntity> findByDescriptionContaining(String filter);
 }

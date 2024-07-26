fdfc7fe6739e9868fc8aa08677b90ba37ba2c06f
Breno Borges <breno.borges.15@outlook.com>
2024-07-26 20:32:16 -0300

    Corrigido Exception do test da Controller de cadastrar vagas. Implementado banco h2 para os testes, adicionando uma application properties de teste

--- src/test/java/br/com/brenoborges/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/brenoborges/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -6,10 +6,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,15 +20,22 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import br.com.brenoborges.gestao_vagas.modules.company.dto.CreateJobDTO;
+import br.com.brenoborges.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.brenoborges.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.com.brenoborges.gestao_vagas.utils.TestUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateJobControllerTest {
 
     private MockMvc mvc; // Instância MockMvc que vai simular as requisições Http
 
+    @Autowired
     private WebApplicationContext context; // Contexto da aplicação web
+
+    @Autowired
+    private CompanyRepository companyRepository;
 
     @Before // Antes de cada método de teste, irá executar esse método.
     // Método para preparar o ambiente de teste para o MockMvc simular as
@@ -42,6 +51,16 @@ public class CreateJobControllerTest {
     @DisplayName("Should be able to create a new job")
     public void shouldBeAbleToCreateANewJob() throws Exception {
 
+        CompanyEntity company = CompanyEntity.builder()
+                .name("COMPANY_NAME")
+                .description("COMPANY_DESCRIPTION")
+                .email("enail@company.com")
+                .username("COMPANY_USERNAME")
+                .password("1234567890")
+                .build();
+
+        this.companyRepository.saveAndFlush(company);
+
         CreateJobDTO createJobDTO = CreateJobDTO.builder()
                 .benefits("BENEFITS_TEST")
                 .description("DESCRIPTION_TESTE")
@@ -51,7 +70,7 @@ public class CreateJobControllerTest {
         mvc.perform(MockMvcRequestBuilders.post("/company/job/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(TestUtils.objectToJSON(createJobDTO))
-                .header("Authorization", TestUtils.generateToekn(UUID.randomUUID(), "JAVACOMPANY_@123#")))
+                .header("Authorization", TestUtils.generateToekn(company.getId(), "JAVACOMPANY_@123#")))
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
     }

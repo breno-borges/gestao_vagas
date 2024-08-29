897f97aa3e96d7127c247e13cecf68085c2051ed
Breno Borges <breno.borges.15@outlook.com>
2024-08-28 22:58:36 -0300

    Refatoração de token de autenticação e implementação de método para listar as vagas que a empresa cadastrou

---
+++ src/main/java/br/com/brenoborges/gestao_vagas/modules/company/useCases/ListAllJobsByCompanyIdUseCase.java
@@ -0,0 +1,21 @@
+package br.com.brenoborges.gestao_vagas.modules.company.useCases;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.brenoborges.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.brenoborges.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class ListAllJobsByCompanyIdUseCase {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public List<JobEntity> execute(UUID companyId) {
+        return this.jobRepository.findByCompanyId(companyId);
+    }
+}
